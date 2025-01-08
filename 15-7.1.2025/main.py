23df523f59c2cea475a7c32b9c9d4b4572601766
Thomasy23 <thomas.mausar@gmail.com>
2025-01-08 23:12:47 +0100

    lekcija 15

---
+++ 15-7.1.2025/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import os
+
+app = Flask (__name__, template_folder=os.path.join(os.path.dirname(__file__), "templates"))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(use_reloader=True)
