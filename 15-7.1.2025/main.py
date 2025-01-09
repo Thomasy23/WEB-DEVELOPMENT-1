2a0e5ae4102f6ac754f979d503cac13a55fccdfa
Thomasy23 <thomas.mausar@gmail.com>
2025-01-09 05:09:21 +0100

    popravek

--- 15-7.1.2025/main.py
+++ 15-7.1.2025/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 
 app = Flask (__name__,)
 
@@ -7,6 +7,12 @@ app = Flask (__name__,)
 def index():
     return render_template("index.html")
 
+
+@app.route("/about")
+def about_me():
+    return render_template("about.html")
+
+
 if __name__ == '__main__':
 
-    app.run(use_reloader=True)
+    app.run(use_reloader=True, port=5002)
